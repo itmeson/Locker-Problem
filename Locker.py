ea0d92c48729693d33e635fa7fc9b649091ae038
John Burk <john.burk98@gmail.com>
2018-11-30 09:59:05 +0100

    initial commit
    Initial commit

---
+++ Locker.py
@@ -0,0 +1,28 @@
+class Locker:
+  locker_counter = 1
+  #this creates a default locker
+  def __init__(self):
+    #set each locker with a unique id
+    self.id = Locker.locker_counter 
+    Locker.locker_counter += 1
+    self.isOpen = False
+    self.flipCount = 0
+    self.students = [] #list of all studnets who flipped this locker
+
+  def flip(self, s):
+    self.isOpen = not self.isOpen
+    self.flipCount += 1
+    self.students.append(s)
+
+  def status(self):
+    #returns a string listing the status of a locker
+    s = str(self.id) + ":"
+    if(self.isOpen):
+      s += " open: "
+    else:
+      s += " closed: "
+    s = s + str(self.flipCount) +" "+  str (self.students)  
+    return s
+
+  def reset(self):
+    locker_counter = 1 
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Locker Problem
+
+This is an Juypter notebook for exploring the locker problem that is a staple of middle school math curriculua.
+
+Run LockerProblem.ipynb to get started.
